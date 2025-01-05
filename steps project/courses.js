3e3933f3e51744c4e67bc67591afe86c8c026d5f
Ahmed Kamal <57872185+AhmedKamalMo@users.noreply.github.com>
2025-01-06 01:24:47 +0200

    Add files via upload

--- steps project/courses.js
+++ steps project/courses.js
@@ -1,50 +1,158 @@
-
-// for ratiing
-
-document.addEventListener('DOMContentLoaded', () => {
-  const ratingStars = document.querySelectorAll('.rating span');
-  
-  ratingStars.forEach((star, index) => {
-      star.addEventListener('click', () => {
-          ratingStars.forEach((s, i) => {
-              if (i <= index) {
-                  s.classList.remove('inactive');
-              } else {
-                  s.classList.add('inactive');
-              }
-          });
-      });
-  });
-});
-
-// var videoDiv = document.getElementById("showVideo");
 var root = document.getElementsByClassName("material")[0];
-// get data from json
+var cardContainer = document.getElementsByClassName("card-container")[0];
 var JsonData;
 var htx = new XMLHttpRequest();
 htx.open("GET", "/data.json");
-htx.onreadystatechange = function () {
-  // console.log("vv");
 
+htx.onreadystatechange = function () {
   if (this.readyState == 4 && this.status == 200) {
     JsonData = JSON.parse(htx.response);
-    // console.log(JsonData);
-    for (let index of JsonData) {
-      // console.log(index.src);
+    Object.keys(JsonData).forEach(function (category) {
+      var dataArray = JsonData[category];
+      dataArray.forEach(function (item) {
+        card = document.createElement("div");
+        card.classList.add("card");
 
-      var div = document.createElement("div");
-      div.classList.add("boox");
-      root.appendChild(div);
-      var video = document.createElement("video");
-      
-      video.src = index.src;
-      video.style.width = 100+'%';
-      video.style.height = 100+'%';
-      video.controls = true;
-      video.poster="./aa.jpg"
-      div.appendChild(video);
-      // console.log(index);
-    }
+        const imagePlaceholder = document.createElement("div");
+        imagePlaceholder.classList.add("image-placeholder");
+        card.appendChild(imagePlaceholder);
+
+        var poster = document.createElement("img");
+        poster.src = item.poster;
+        poster.classList.add("poster");
+        poster.style.cursor = "pointer";
+        poster.style.width = "100%";
+        poster.style.height = "100%";
+        imagePlaceholder.appendChild(poster);
+
+        const cardContent = document.createElement("div");
+        cardContent.classList.add("card-content");
+        card.appendChild(cardContent);
+
+        const cardHead = document.createElement("div");
+        cardHead.classList.add("card-head");
+        cardContent.appendChild(cardHead);
+
+        const cardTitle = document.createElement("h5");
+        cardTitle.textContent = item.title;
+        cardHead.appendChild(cardTitle);
+
+        const rating = document.createElement("div");
+        rating.classList.add("rating");
+
+        for (let i = 0; i < 5; i++) {
+          const star = document.createElement("span");
+          star.innerHTML = "&#9733;";
+          if (i >= 3) {
+            star.classList.add("inactive");
+          }
+          rating.appendChild(star);
+        }
+        cardHead.appendChild(rating);
+
+        const paragraph = document.createElement("p");
+        paragraph.textContent =
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.";
+        cardContent.appendChild(paragraph);
+
+        const enrollButton = document.createElement("a");
+        enrollButton.href = "#";
+        enrollButton.classList.add("enroll");
+        enrollButton.textContent = "enroll now";
+        cardContent.appendChild(enrollButton);
+        cardContainer.appendChild(card);
+        enrollButton.addEventListener("click", function () {
+          var videoPageUrl = `/incourse.html?title=${encodeURIComponent(
+            item.title
+          )}&src=${encodeURIComponent(item.src)}&category=${encodeURIComponent(
+            category
+          )}`;
+          window.location.href = videoPageUrl;
+        });
+      });
+    });
   }
 };
 htx.send();
+
+function selected(index) {
+  cardContainer.innerHTML = "";
+  var category = Object.keys(JsonData)[index];
+  var courses = JsonData[category];
+  console.log(courses);
+
+  var buttons = document.querySelectorAll(".categories button");
+  buttons.forEach(function (button, idx) {
+    button.style.backgroundColor = "";
+    button.style.color = "#ac7979";
+
+    if (idx === index) {
+      button.style.backgroundColor = "#4CAF50";
+      button.style.color = "white";
+    }
+  });
+
+  courses.forEach(function (item) {
+    card = document.createElement("div");
+    card.classList.add("card");
+
+    const imagePlaceholder = document.createElement("div");
+    imagePlaceholder.classList.add("image-placeholder");
+    card.appendChild(imagePlaceholder);
+
+    var poster = document.createElement("img");
+    poster.src = item.poster;
+    poster.classList.add("poster");
+    poster.style.cursor = "pointer";
+    poster.style.width = "100%";
+    poster.style.height = "100%";
+    poster.style.borderRadius = "10px";
+    imagePlaceholder.appendChild(poster);
+
+    const cardContent = document.createElement("div");
+    cardContent.classList.add("card-content");
+    card.appendChild(cardContent);
+
+    const cardHead = document.createElement("div");
+    cardHead.classList.add("card-head");
+    cardContent.appendChild(cardHead);
+
+    const cardTitle = document.createElement("h5");
+    cardTitle.textContent = item.title;
+    cardHead.appendChild(cardTitle);
+
+    const rating = document.createElement("div");
+    rating.classList.add("rating");
+
+    for (let i = 0; i < 5; i++) {
+      const star = document.createElement("span");
+      star.innerHTML = "&#9733;";
+      if (i >= 3) {
+        star.classList.add("inactive");
+      }
+      rating.appendChild(star);
+    }
+    cardHead.appendChild(rating);
+
+    const paragraph = document.createElement("p");
+    paragraph.textContent =
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.";
+    cardContent.appendChild(paragraph);
+
+    const enrollButton = document.createElement("a");
+    enrollButton.href = "#";
+    enrollButton.classList.add("enroll");
+    enrollButton.textContent = "enroll now";
+    cardContent.appendChild(enrollButton);
+    cardContainer.appendChild(card);
+
+    enrollButton.addEventListener("click", function () {
+      var videoPageUrl = `/incourse.html?title=${encodeURIComponent(
+        item.title
+      )}&src=${encodeURIComponent(item.src)}&category=${encodeURIComponent(
+        category
+      )}`;
+      window.location.href = videoPageUrl;
+    });
+  });
+}
---
+++ steps project/incourse.js
@@ -0,0 +1,142 @@
+var mainContent = document.getElementsByClassName("main-content")[0];
+var videoPlayer = document.getElementsByClassName("video-player")[0];
+var sidebar = document.getElementsByClassName("sidebar")[0];
+var params = new URLSearchParams(window.location.search);
+var mainTitle = params.get("title");
+var mainSrc = params.get("src");
+var category = params.get("category");
+var count = 0;
+var htx = new XMLHttpRequest();
+htx.open("GET", "/data.json");
+
+htx.onreadystatechange = function () {
+  if (this.readyState == 4 && this.status == 200) {
+    JsonData = JSON.parse(htx.response);
+
+    var mainVideo = document.createElement("iframe");
+    mainVideo.src = mainSrc;
+    mainVideo.style.height = "100%";
+    mainVideo.style.width = "100%";
+    videoPlayer.appendChild(mainVideo);
+
+    document.getElementById("category").textContent = category;
+    document.getElementById("lesson").textContent = mainTitle;
+
+    if (JsonData[category]) {
+      var c = 0;
+      var relatedVideos = JsonData[category].filter(function (item) {
+        c++;
+        if (item.title == mainTitle) count = c - 1;
+        return item.title !== mainTitle;
+      });
+
+      relatedVideos.forEach(function (item) {
+        var lesson = document.createElement("div");
+        lesson.classList.add("lesson");
+        sidebar.appendChild(lesson);
+        var poster = document.createElement("img");
+        poster.classList.add("lesson-thumbnail");
+        poster.src = item.poster;
+        poster.alt = item.title;
+
+        var title = document.createElement("h3");
+        title.textContent = item.title;
+        title.style.fontSize = "15px";
+        title.classList.add("lesson-title");
+
+        poster.addEventListener("click", function () {
+          var videoPageUrl = `/incourse.html?title=${encodeURIComponent(
+            item.title
+          )}&src=${encodeURIComponent(item.src)}&category=${encodeURIComponent(
+            category
+          )}`;
+          window.location.href = videoPageUrl;
+        });
+
+        lesson.appendChild(poster);
+        lesson.appendChild(title);
+      });
+    }
+  }
+};
+htx.send();
+
+//
+var timer = 30;
+var score = 0;
+var interval;
+var questionIndex = 0;
+var divtimer = document.getElementById("timer");
+var practice = document.getElementById("practice-content");
+// var bu
+function startQuiz() {
+  practice.innerHTML = "";
+  divtimer.innerHTML = "";
+  console.log(count);
+
+  var timerElement = document.createElement("div");
+  timerElement.id = "timer";
+  timerElement.textContent = `Time left: ${timer} seconds`;
+  divtimer.appendChild(timerElement);
+  interval = setInterval(() => {
+    console.log(timer);
+
+    timer--;
+    timerElement.textContent = `Time left: ${timer} seconds`;
+    if (timer <= 0) {
+      clearInterval(interval);
+      endQuiz();
+    }
+  }, 1000);
+
+  showQuestion();
+}
+
+function showQuestion() {
+  var v = JsonData[category];
+  var quest = v[count] && v[count].quiz;
+
+  if (quest && quest.length > 0) {
+    practice.innerHTML = "";
+
+    if (questionIndex < quest.length) {
+      var question = quest[questionIndex];
+
+      if (question) {
+        var questionElement = document.createElement("p");
+        questionElement.textContent = question.question;
+        practice.appendChild(questionElement);
+
+        question.options.forEach((option) => {
+          var button = document.createElement("button");
+          button.textContent = option;
+          button.style.background='green';
+          button.style.margin='5px';
+          button.onclick = function () {
+            if (option === question.correct) score++;
+            questionIndex++;
+            showQuestion();
+          };
+          practice.appendChild(button);
+        });
+      } else {
+        endQuiz();
+      }
+    } else {
+      endQuiz();
+    }
+  } else {
+    endQuiz();
+  }
+}
+
+function endQuiz() {
+  clearInterval(interval);
+  var v = JsonData[category];
+  practice.innerHTML = `Your score: ${score}/${questionIndex} <br> ${
+    score >= Math.ceil(questionIndex * 0.6) ? "Congratulations!" : "Try again!"
+  }`;
+  questionIndex = 0;
+  score = 0;
+  timer = 30;
+}
