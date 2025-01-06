cb9c8640d071a40cdadb6f81b067a0c8992077ce
Ahmed Kamal <57872185+AhmedKamalMo@users.noreply.github.com>
2025-01-07 00:21:37 +0200

    Add files via upload

--- Courses/courses.js
+++ Courses/courses.js
@@ -7,79 +7,24 @@ htx.open("GET", "../data/data.json");
 htx.onreadystatechange = function () {
   if (this.readyState == 4 && this.status == 200) {
     JsonData = JSON.parse(htx.response);
+    console.log(JsonData);
+
     Object.keys(JsonData).forEach(function (category) {
       var dataArray = JsonData[category];
-      dataArray.forEach(function (item) {
-        card = document.createElement("div");
-        card.classList.add("card");
-
-        const imagePlaceholder = document.createElement("div");
-        imagePlaceholder.classList.add("image-placeholder");
-        card.appendChild(imagePlaceholder);
-
-        var poster = document.createElement("img");
-        poster.src = "../assets/courses/" +item.poster;
-        poster.classList.add("poster");
-        poster.style.cursor = "pointer";
-        poster.style.width = "100%";
-        poster.style.height = "100%";
-        imagePlaceholder.appendChild(poster);
-
-        const cardContent = document.createElement("div");
-        cardContent.classList.add("card-content");
-        card.appendChild(cardContent);
-
-        const cardHead = document.createElement("div");
-        cardHead.classList.add("card-head");
-        cardContent.appendChild(cardHead);
-
-        const cardTitle = document.createElement("h5");
-        cardTitle.textContent = item.title;
-        cardHead.appendChild(cardTitle);
-
-        const rating = document.createElement("div");
-        rating.classList.add("rating");
-
-        for (let i = 0; i < 5; i++) {
-          const star = document.createElement("span");
-          star.innerHTML = "&#9733;";
-          if (i >= 3) {
-            star.classList.add("inactive");
-          }
-          rating.appendChild(star);
-        }
-        cardHead.appendChild(rating);
-
-        const paragraph = document.createElement("p");
-        paragraph.textContent =
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.";
-        cardContent.appendChild(paragraph);
-
-        const enrollButton = document.createElement("a");
-        enrollButton.href = "#";
-        enrollButton.classList.add("enroll");
-        enrollButton.textContent = "enroll now";
-        cardContent.appendChild(enrollButton);
-        cardContainer.appendChild(card);
-        enrollButton.addEventListener("click", function () {
-          var videoPageUrl = `./incourse.html?title=${encodeURIComponent(
-            item.title
-          )}&src=${encodeURIComponent(item.src)}&category=${encodeURIComponent(
-            category
-          )}`;
-          window.location.href = videoPageUrl;
-        });
-      });
+      data(dataArray, category);
     });
   }
 };
 htx.send();
 
-function selected(index) {
+function selected(id, index) {
   cardContainer.innerHTML = "";
-  var category = Object.keys(JsonData)[index];
-  var courses = JsonData[category];
-  console.log(courses);
+  Object.keys(JsonData).forEach(function (category) {
+    var dataArray = JsonData[category];
+    if (dataArray.id == id) {
+      data(dataArray, category);
+    }
+  });
 
   var buttons = document.querySelectorAll(".categories button");
   buttons.forEach(function (button, idx) {
@@ -91,68 +36,68 @@ function selected(index) {
       button.style.color = "white";
     }
   });
+}
 
-  courses.forEach(function (item) {
-    card = document.createElement("div");
-    card.classList.add("card");
+function data(dataArray, category) {
+  card = document.createElement("div");
+  card.classList.add("card");
+  console.log(dataArray.description);
+  console.log(dataArray.img);
 
-    const imagePlaceholder = document.createElement("div");
-    imagePlaceholder.classList.add("image-placeholder");
-    card.appendChild(imagePlaceholder);
+  const imagePlaceholder = document.createElement("div");
+  imagePlaceholder.classList.add("image-placeholder");
+  card.appendChild(imagePlaceholder);
 
-    var poster = document.createElement("img");
-    poster.src = "../assets/courses/"+item.poster;
-    poster.classList.add("poster");
-    poster.style.cursor = "pointer";
-    poster.style.width = "100%";
-    poster.style.height = "100%";
-    poster.style.borderRadius = "10px";
-    imagePlaceholder.appendChild(poster);
+  var poster = document.createElement("img");
+  poster.src = dataArray.img;
+  poster.classList.add("poster");
+  poster.style.cursor = "pointer";
+  poster.style.width = "100%";
+  poster.style.height = "100%";
+  imagePlaceholder.appendChild(poster);
 
-    const cardContent = document.createElement("div");
-    cardContent.classList.add("card-content");
-    card.appendChild(cardContent);
+  const cardContent = document.createElement("div");
+  cardContent.classList.add("card-content");
+  card.appendChild(cardContent);
 
-    const cardHead = document.createElement("div");
-    cardHead.classList.add("card-head");
-    cardContent.appendChild(cardHead);
+  const cardHead = document.createElement("div");
+  cardHead.classList.add("card-head");
+  cardContent.appendChild(cardHead);
 
-    const cardTitle = document.createElement("h5");
-    cardTitle.textContent = item.title;
-    cardHead.appendChild(cardTitle);
+  const cardTitle = document.createElement("h5");
+  cardTitle.textContent = dataArray.id;
+  cardHead.appendChild(cardTitle);
 
-    const rating = document.createElement("div");
-    rating.classList.add("rating");
+  const rating = document.createElement("div");
+  rating.classList.add("rating");
 
-    for (let i = 0; i < 5; i++) {
-      const star = document.createElement("span");
-      star.innerHTML = "&#9733;";
-      if (i >= 3) {
-        star.classList.add("inactive");
-      }
-      rating.appendChild(star);
+  for (let i = 0; i < 5; i++) {
+    const star = document.createElement("span");
+    star.innerHTML = "&#9733;";
+    if (i >= 3) {
+      star.classList.add("inactive");
     }
-    cardHead.appendChild(rating);
+    rating.appendChild(star);
+  }
+  cardHead.appendChild(rating);
 
-    const paragraph = document.createElement("p");
-    paragraph.textContent =
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.";
-    cardContent.appendChild(paragraph);
+  const paragraph = document.createElement("p");
+  paragraph.textContent = dataArray.description;
+  cardContent.appendChild(paragraph);
 
-    const enrollButton = document.createElement("a");
-    enrollButton.href = "#";
-    enrollButton.classList.add("enroll");
-    enrollButton.textContent = "enroll now";
-    cardContent.appendChild(enrollButton);
-    cardContainer.appendChild(card);
+  const enrollButton = document.createElement("a");
+  enrollButton.href = "#";
+  enrollButton.classList.add("enroll");
+  enrollButton.textContent = "enroll now";
+  cardContent.appendChild(enrollButton);
+  cardContainer.appendChild(card);
 
-    enrollButton.addEventListener("click", function () {
-      var videoPageUrl = `./incourse.html?title=${encodeURIComponent(
-        item.title
-      )}&src=${encodeURIComponent(item.src)}&category=${encodeURIComponent(
-        category
-      )}`;
-      window.location.href = videoPageUrl;
-    });
-  });
+  // enrollButton.addEventListener("click", function () {
+  //   var videoPageUrl = `/incourse.html?title=${encodeURIComponent(
+  //     dataArray.id
+  //   )}&src=${encodeURIComponent(
+  //     "https://drive.google.com/file/d/1dT7HvbwPxnKWKh6iUyV-jkz9yb8wPCdq/preview"
+  //   )}&category=${encodeURIComponent(category)}`;
+  //   window.location.href = videoPageUrl;
+  // });
 }
