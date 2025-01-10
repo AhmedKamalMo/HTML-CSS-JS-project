eed8cd6a31b19cec20371906f27e548a61467cb3
Mark <markmamdoh9@gmail.com>
2025-01-10 19:08:35 +0200

    fixed forget password

--- forgetPassword/forget.js
+++ forgetPassword/forget.js
@@ -1,28 +1,30 @@
+// Function to get users from localStorage
 function getlocal() {
     var users = JSON.parse(localStorage.getItem("users")) || [];
     return { users };
 }
 
+// Function to handle "Forgot Password" logic
 function getpassword() {
-    var email = document.getElementById("emailinput").value;
+    var email = document.getElementById("emailinput").value; // Get the email input value
 
+    var { users } = getlocal(); // Retrieve users from localStorage
 
-    var { users } = getlocal();
+    // Find user by email
+    let user = users.find(user => user.email === email);
 
-    //find user by email
-    let user = users.find(user=>user.email===email)
-
-    if (user ) {
+    if (user) {
         // If the email exists, show the corresponding password
         document.getElementById("show").innerText =
-            "Your password is: " + user.password;
+            "Your password is: " + user.pass;  // Use user.pass, not user.password
     } else {
         // If the email does not exist
         document.getElementById("show").innerText =
-            "There is no email with this name";
+            "There email address is not registered";
     }
 }
 
+// Redirect to login page
 function gologein() {
-    location.href = "../login/login.html"
+    location.href = "../login/login.html";  // Go to login page
 }
